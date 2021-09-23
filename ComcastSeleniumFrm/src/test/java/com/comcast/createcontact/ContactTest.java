bd17e7f9c7dce780b994001898a7c2823636fbef
Shivani319 <shivanisinha319@gmail.com>
2021-09-23 15:19:35 +0530

    File Commit

--- ComcastSeleniumFrm/src/test/java/com/comcast/createcontact/ContactTest.java
+++ ComcastSeleniumFrm/src/test/java/com/comcast/createcontact/ContactTest.java
@@ -5,6 +5,10 @@ import org.testng.annotations.Test;
 public class ContactTest {
 	@Test(groups="smoke")
 	public void createContactTest() {
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		System.out.println(URL);
+		System.out.println(BROWSER);
 		System.out.println("execute createContactTest");
 	}
 	@Test(groups="regression")
